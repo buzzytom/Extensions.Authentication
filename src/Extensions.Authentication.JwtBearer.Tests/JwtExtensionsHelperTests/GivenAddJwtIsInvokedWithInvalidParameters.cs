18e6f6c3932bafca6cec8b3c94096c5b5b61bd6d
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-13 22:27:30 +0100

    Implemented unit test in the Extensions.Authentication.JwtBearer.Tests project.

---
+++ src/Extensions.Authentication.JwtBearer.Tests/JwtExtensionsHelperTests/GivenAddJwtIsInvokedWithInvalidParameters.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Extensions.Authentication.JwtBearer.Tests
+{
+    [TestFixture]
+    public class GivenAddJwtIsInvokedWithInvalidParameters
+    {
+        [Test]
+        public void WithoutAServiceCollection_ThenAnErrorIsThrown()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => JwtExtensionsHelper.AddJwt(null, new JwtConfiguration()));
+            Assert.AreEqual("services", exception.ParamName);
+        }
+
+        [Test]
+        public void WithoutAConfiguration_ThenAnErrorIsThrown()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => JwtExtensionsHelper.AddJwt(Mock.Of<IServiceCollection>(), null));
+            Assert.AreEqual("configuration", exception.ParamName);
+        }
+    }
+}
