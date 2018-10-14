6a3b85e5adb3fc61020b9f9db2d7503de3e7633b
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-14 11:21:21 +0100

    Added unit tests for AddTokenService of ExtensionsHelper.cs. Removed the now redundant Microsoft.Extensions.DependencyInjection package from Extensions.Authentication.Api project.

---
+++ src/Extensions.Authentication.Tests/ExtensionsHelperTests/GivenAddTokenServiceIsInvoked.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Extensions.Authentication.Tests.ExtensionsHelperTests
+{
+    [TestFixture]
+    public class GivenAddTokenServiceIsInvoked
+    {
+        [Test]
+        public void WithoutAServiceCollection_ThenAnExceptionIsThrown()
+        {
+            ArgumentNullException exception = Assert.Throws<ArgumentNullException>(() => ExtensionsHelper.AddTokenService(null));
+            Assert.AreEqual("services", exception.ParamName);
+        }
+
+        [Test]
+        public void WithAServiceCollection_ThenATokenServiceIsAdded()
+        {
+            Mock<IServiceCollection> services = new Mock<IServiceCollection>();
+
+            ExtensionsHelper.AddTokenService(services.Object);
+
+            services.Verify(instance => instance.Add(It.Is<ServiceDescriptor>(x =>
+                x.Lifetime == ServiceLifetime.Scoped &&
+                x.ServiceType == typeof(ITokenService) &&
+                x.ImplementationType == typeof(TokenService))), Times.Once);
+        }
+    }
+}
