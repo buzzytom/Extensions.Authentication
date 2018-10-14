d326f7a1a8a3972e7e35b998cb3038c4ee912bc4
Thomas Williams <thomas7.williams@hotmail.com>
2018-10-14 12:53:32 +0100

    Wrote unit tests for the TokenService.

---
+++ src/Extensions.Authentication.Tests/TokenServiceTests/GivenGenerateSaltIsInvoked.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace Extensions.Authentication.Tests.TokenServiceTests
+{
+    [TestFixture]
+    public class GivenGenerateSaltIsInvoked
+    {
+        private TokenService subject = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            subject = new TokenService();
+        }
+
+        [Test]
+        public void ThenTheGeneratedValueIsAReasonableLength()
+        {
+            string salt = subject.GenerateSalt();
+            Assert.IsTrue(salt.Length >= 16 && salt.Length < 128);
+        }
+
+        [Test]
+        public void ThenGeneratedSaltsAreDistinct()
+        {
+            string salt = subject.GenerateSalt();
+            for (int i = 0; i < 100; i++)
+                Assert.AreNotEqual(salt, subject.GenerateSalt());
+        }
+
+        [Test]
+        public void ThenAllGeneratedSaltsAreTheSameLength()
+        {
+            string salt = subject.GenerateSalt();
+            for (int i = 0; i < 100; i++)
+                Assert.AreEqual(salt.Length, subject.GenerateSalt().Length);
+        }
+    }
+}
---
+++ src/Extensions.Authentication.Tests/TokenServiceTests/GiventHashIsInvoked.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Extensions.Authentication.Tests
+{
+    [TestFixture]
+    public class GivenHashIsInvoked
+    {
+        private TokenService subject = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            subject = new TokenService();
+        }
+
+        [Test]
+        public void ThenTheSameParametersProduceTheSameHash()
+        {
+            string salt = subject.GenerateSalt();
+            string hashA = subject.Hash("some value", salt);
+            string hashB = subject.Hash("some value", salt);
+            Assert.AreEqual(hashA, hashB);
+        }
+
+        [Test]
+        public void ThenDifferentValuesProduceDistrinctResults()
+        {
+            string salt = subject.GenerateSalt();
+            string hash = subject.Hash("some value", salt);
+            for (int i = 0; i < 20; i++)
+                Assert.AreNotEqual(hash, subject.Hash("some value " + i, salt));
+        }
+
+        [Test]
+        public void ThenDifferentSaltsProduceDistrinctResults()
+        {
+            string salt = subject.GenerateSalt();
+            string hash = subject.Hash("some value", salt);
+            for (int i = 0; i < 20; i++)
+                Assert.AreNotEqual(hash, subject.Hash("some value", subject.GenerateSalt()));
+        }
+    }
+}
